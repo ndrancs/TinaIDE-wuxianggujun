ccd9674d05ff23ad78692a651584e3066c244400
wuxianggujun <3344207732@qq.com>
2025-11-01 16:31:58 +0800

    终于，可以进入终端，但是无法输入内容等！

--- external/termux-app/termux-shared/src/main/cpp/Android.mk
+++ external/termux-app/termux-shared/src/main/cpp/Android.mk
@@ -3,4 +3,6 @@ include $(CLEAR_VARS)
 LOCAL_LDLIBS := -llog
 LOCAL_MODULE := local-socket
 LOCAL_SRC_FILES := local-socket.cpp
+# Ensure 16KB page-size compatibility on Android 15+ devices
+LOCAL_LDFLAGS += -Wl,-z,max-page-size=16384
 include $(BUILD_SHARED_LIBRARY)
